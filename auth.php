62e6be7b1d671d61bc104f63f2d28650c5f623f9
choheejin <hjcho9510@gmail.com>
2022-11-29 20:38:14 +0900

    구조적으로 변경

---
+++ auth.php
@@ -0,0 +1,21 @@
+<?php
+include_once 'dbconfig.php';
+include_once 'app.php';
+
+
+$app = new App();
+if ($app->post('/signup')) {
+
+    // URL 파라미터 구해오기(정규식부분에서 괄호 부분 순서대로 가져옴)
+    $params = $app->getParams();
+
+    // POST, PUT 등에서 보내온 데이타
+    $data = $app->getData();
+
+    // 결과출력
+    $app->print(array(
+        'user_id' => $data['user_id'],
+        'password' => $data['password']
+    ));
+}
+?>
--- dbconfig.php
+++ dbconfig.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost:3306";
 $username = "root";
-$password = "비밀번호";
+$password = "비밀번호 변경";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password);
