6771a37f3394e5fcf64ad1d786c682158568500d
Yeons <74572293+Yeons0110@users.noreply.github.com>
2022-12-18 13:36:41 +0900

    출석체크 확인 backend

--- auth.php
+++ auth.php
@@ -44,45 +44,43 @@ if ($app->post('/login')) {
     $sql2 = "SELECT count(*) as 'cnt' from user where id ='$id' and pw ='$pw';";
     $stmt = $conn->prepare($sql2);
     $stmt->execute();
-
+ 
     if($stmt->execute()) {
-        $result = $stmt->fetchAll();
-        echo "test1:"
-        if($result[0][0] == 1) {
-            $response = ['status' => 200, 'message' => 'Login successfully', 'response' => $id];
+        $result = $stmt->fetch();
+        
+        if($result[0]){
+            $sqlDate = "select attendance_point from point where date_format(attendance_date,'%Y-%m-%d') = date_format(now(),'%Y-%m-%d')and user_id like '$id'";
+            $stmt2 = $conn->prepare($sqlDate);
+            $stmt2->execute();
+            $result2 = $stmt2->fetch();
+        
+            if($result2){
+                $response = ['status' => 200, 'message' => 'Login successfully and have an attendance', 'response' => $id];
+                $app->print($response);
+            } else {
+                $sqlAttend = "INSERT INTO point (attendance_point, user_id) VALUES(1, '".$id."')";
+                $stmt3 = $conn->prepare($sqlAttend);
+                if($stmt3->execute()){
+                    // 오늘 출석일 가지고 오는 쿼리문
+                    $sql3 = "select * from point where date_format(attendance_date,'%Y-%m-%d') = date_format(now(),'%Y-%m-%d')and user_id like'".$id."'";
+                    $stmt4 = $conn->prepare($sql3);
+                    if($stmt4->execute()){
+                        $result3 = $stmt4->fetch();
+                        
+                        $data = array(
+                            'point_id' => $result3['point_id'],
+                            'user_id' => $result3['user_id'],
+                            'attendance_date' => $result3['attendance_date'],
+                            'attendance_point' => $result3['attendance_point']
+                        );
 
-            $sqlTime = "SELECT attendance_point FROM cattmunity.point where date_format(attendance_date,'%Y-%m-%d')= date_format(now(),'%Y-%m-%d')and user_id like '$id';";
-            $stmtTime = $conn->prepare($sqlTime);
-            $stmtTime->execute();
-            if($stmt->execute()) {
-                $resultTime = $stmtTime->fetchAll();
-                echo "test1:" + $sqlTime;
-            //  if($resultTime==""){
-            //     echo $sqlTime;
-            //  }
+                        $response = ['status' => 200, 'message' => 'Login successfully and make an attendance', 'response' => $data];
+                        $app->print($response);
+                    }
+                }                
             }
-            // 출석체크
-            // if(로그인 처음(1)이라는 포인트가 들어오면) 쿼리문
-            
-            $sqlAttend = "INSERT INTO point (attendance_point, attendance_date, user_id) VALUES(:attendance_point, :attendance_date, :user_id)";
-            $stmtAttend = $conn->prepare($sqlAttend);
-
-            $stmtAttend->bindParam(':attendance_point', $user['attendance_point']);
-            $stmtAttend->bindParam(':attendance_date', $user['attendance_date']);
-            $stmtAttend->bindParam(':user_id', $user['user_id']);
-
-            $app->print($response);
-        } else {
-            $response = ['status' => 500, 'message' => 'Login failed'];
-            $app->print($response, 500);
         }
-
-    } else {
-        $response = ['status' => 500, 'message' => 'Login failed'];
-        $app->print($response, 500);
     }
-
-   
 
 }
 
@@ -117,6 +115,38 @@ if ($app->get('/my-page/([a-zA-Z0-9_]*)')) {
         $response = ['status' => 500, 'message' => 'getmypage failed'];
         $app->print($response, 500);
 }
+
+}
+
+if ($app->get('/mystamp/([a-zA-Z0-9_]*)')) {
+    // POST, PUT 등에서 보내온 데이타
+    $params = $app->getParams();
+    
+    $sql4 = "SELECT attendance_date from point where user_id ='$params[0]';";
+    $stmt = $conn->prepare($sql4);
+    $stmt->execute();
+
+    if($stmt->execute()) {
+        $data = [];
+        while($row = $stmt->fetch()){
+            array_push($data, array(
+                'attendance_date' => $row['attendance_date']
+            ));
+        }
+
+        if(count($data) > 0) {
+            $response = ['status' => 200, 'message' => 'mystemp successfully', 'response' => $data[0]];
+            $app->print($response);
+        } else {
+            $response = ['status' => 500, 'message' => 'getmystemp failed'];
+            $app->print($response, 500);
+        }
+
+    } else {
+        $response = ['status' => 500, 'message' => 'getmypage failed'];
+        $app->print($response, 500);
+}
+
 }
 
 ?>
